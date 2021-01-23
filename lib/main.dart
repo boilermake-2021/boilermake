3a1eda743a127b5d172f5e468318e8f96a0ab435
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-23 12:07:11 -0500

    Added some functionality in BudgetService

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:boilermake/screens/start/start_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
@@ -11,23 +12,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    http.post('http://api.nessieisreal.com/customers?key=ff20c7789770dbd08b2b18467a84a68e',
-      headers: {
-        'Content-type' : 'application/json'
-      },
-      body: json.encode ({
-        "first_name": "Michael",
-        "last_name": "Dressner",
-        "address": {
-          "street_number": "5717",
-          "street_name": "Elm Street",
-          "city": "Washington",
-          "state": "DC",
-          "zip": "20817"
-        }}
-      )).then((Response r) {
-        print(r.body);
-    });
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
