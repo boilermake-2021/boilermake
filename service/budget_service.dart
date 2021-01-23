80a76046c6c62a659ccf39148e2924702414cf23
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-23 13:29:09 -0500

    Made constant static and deleted unnecessary directory

--- service/budget_service.dart
+++
@@ -1,5 +0,0 @@
-import 'package:boilermake/models/budget_model.dart';
-
-class BudgetService {
-  BudgetModel budgetModel;
-}
--- service/constants.dart
+++
@@ -1,3 +0,0 @@
-class Constants {
-  final String nessie_api_key = "ff20c7789770dbd08b2b18467a84a68e";
-}
