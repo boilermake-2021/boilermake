c2308fb1d19ddd28df19a851e5d216bd77cf7d00
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-23 09:53:48 -0500

    Working on sending a simple post request

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +12,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    http.post('http://api.nessieisreal.com/customers?key=ff20c7789770dbd08b2b18467a84a68e',
+      body: json.encode ({
+        "first_name": "Michael",
+        "last_name": "Dressner",
+        "address": {
+          "street_number": "5717",
+          "street_name": "Elm Street",
+          "city": "Washington",
+          "state": "DC",
+          "zip": "20817"
+        }}
+      )).then((Response r) {
+        print(r.body);
+    });
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
