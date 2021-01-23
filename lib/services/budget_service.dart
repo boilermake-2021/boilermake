3f6a79486cf2056cbde162a4a895fe2562205ef7
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-23 13:02:19 -0500

    Added BudgetService class

--- lib/services/budget_service.dart
+++ lib/services/budget_service.dart
@@ -1,1 +1,9 @@
-class BudgetService {}
+import 'package:boilermake/models/budget_model.dart';
+
+class BudgetService {
+  BudgetModel budgetModel;
+
+  BudgetService() {
+    budgetModel = new BudgetModel();
+  }
+}
