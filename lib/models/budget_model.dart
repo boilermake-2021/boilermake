656790c0389d437836a71e464ae11fec27cfa857
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-23 13:01:39 -0500

    Fixed BudgetModel constructor

--- lib/models/budget_model.dart
+++ lib/models/budget_model.dart
@@ -1,10 +1,42 @@
 import 'package:boilermake/models/budget_category_model.dart';
-import 'package:boilermake/models/loan_model.dart';
 import 'package:money2/money2.dart';
 
 class BudgetModel {
-  List<LoanModel> debts;
-  List<BudgetCategoryModel> categories;
-  Money monthlyIncome;
-  Money moneySpent;
+  Map<String, BudgetCategoryModel> categoryBudgets;
+  Money totalSpending;
+
+  BudgetModel() {
+    totalSpending = Money.fromInt(0, CommonCurrencies().usd);
+  }
+
+  Money getSpendingInCategory(String category) {
+    if (!categoryBudgets.containsKey(category))
+      return Money.fromInt(0, CommonCurrencies().usd);
+    else
+      return categoryBudgets[category].amountSpent;
+  }
+
+  void addSpendingToCategories(Money amount, List<String> categories) {
+    totalSpending += amount;
+
+    for (String category in categories)
+      _addSpendingInCategory(amount, category);
+  }
+
+  void _addSpendingInCategory(Money amount, String category) {
+    if (!categoryBudgets.containsKey(category)) {
+      categoryBudgets[category] = new BudgetCategoryModel();
+      categoryBudgets[category].amountSpent = amount;
+    }
+    else
+      categoryBudgets[category].amountSpent += amount;
+  }
+
+  bool isMaxxed(String category) {
+    if (!categoryBudgets.containsKey(category))
+      return false;
+    else
+      return categoryBudgets[category].amountSpent >=
+          categoryBudgets[category].limit;
+  }
 }
